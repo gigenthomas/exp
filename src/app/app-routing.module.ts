bfe84d458b790c961e4d4264266c36fdc7ffa6b9
Gigen <gigenthomas@gmail.com>
2017-09-17 21:09:08 -0700

    integrate d3 v4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import {JokeComponent} from "./joke.component";
 import {JokeListComponent} from "./jokelist.component";
+import { BrushZoom2Component } from './brushzoom2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes',     component: HeroesComponent },
-  { path: 'joke',     component: JokeListComponent }
+  { path: 'joke',     component: JokeListComponent },
+  { path: 'd3',     component: BrushZoom2Component }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -11,12 +11,15 @@ import {AppRoutingModule} from "./app-routing.module";
 import {JokeComponent} from "./joke.component";
 import {JokeListComponent} from "./jokelist.component";
 import {JokeFormComponent} from "./jakeform.component";
+import { D3Service } from 'd3-ng2-service';
+import { BrushZoom2Component } from './brushzoom2.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
@@ -25,9 +28,13 @@ import {JokeFormComponent} from "./jakeform.component";
     HeroesComponent,
     JokeComponent,
     JokeListComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    BrushZoom2Component
   ],
-  providers: [ HeroService ],
+  providers: [ HeroService , D3Service],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
