18c1f6e71baf70e874ab0e33d0bfc9569f0d91a0
Gigen <gigenthomas@gmail.com>
2017-09-19 06:30:24 -0700

    adding reatvie forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import {JokeComponent} from "./joke.component";
 import {JokeListComponent} from "./jokelist.component";
 import { BrushZoom2Component } from './brushzoom2.component';
+import { D3TreeComponent } from './d3tree.component';
+import { SignupFormComponent } from './signupform.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -14,7 +16,9 @@ const routes: Routes = [
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes',     component: HeroesComponent },
   { path: 'joke',     component: JokeListComponent },
-  { path: 'd3',     component: BrushZoom2Component }
+  { path: 'd3',     component: BrushZoom2Component },
+  { path: 'd3',     component: D3TreeComponent },
+  { path: 'signupform',     component: SignupFormComponent }
 ];
 
 @NgModule({
